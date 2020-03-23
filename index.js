03eccf2f900d59903ca9daac0b1127c2ba2a073b
Jaycee Chow <2832203391@qq.com>
2020-03-23 23:02:41 +0800

    修复一些已知bug，完善文档

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 const command = require("child_process");
 const fs = require("fs");
+const config = require("./config.json");
+let { input, output } = config;
 
-fs.readFile("config.json", (err, data) => {
-    if (err) {
-        console.err(err);
-        return;
-    } else {
-        let { input, output } = JSON.parse(data);
-        command.exec(
-            `node lib/command/j2m.js -i ${input} -o ${output}`,
-            (error, stdout, stderr) => {
-                if (error) {
-                    console.log(error);
-                    return;
-                }
-                console.log(stdout);
-                console.error(stderr);
-            }
-        );
+command.exec(
+    `node lib/command/j2m.js -i data/${input} -o data/${output}`,
+    (error, stdout, stderr) => {
+        if (error) {
+            console.log(error);
+            return;
+        }
+        console.log(stdout);
+        console.error(stderr);
     }
-});
+);
