8431ab1b0f44c2b6d25b26c16df3b5e267c5d02f
Jaycee Chow <2832203391@qq.com>
2020-04-11 20:20:24 +0800

    v0.1.3(beta)

--- index.js
+++
@@ -1,7 +0,0 @@
-const command = require("child_process");
-const fs = require("fs");
-const transform = require("./lib/transform");
-const options = require("./lib/");
-
-// transform.toMarkdown(`data/data.json`);
-console.log(options.replaceExt("data.md",".md"));
--- test/dir_options.test.js
+++ test/dir_options.test.js
@@ -1,64 +1,66 @@
-const options = require("../lib/middleware/file_option");
+const fileops = require("../lib/middleware/fileops");
 const chai = require("chai"),
     expect = chai.expect;
 
 before(() => console.info("开始单元测试:"));
 
-describe("模块测试 dir_options", () => {
+describe("模块测试 dir_fileops", () => {
     describe("函数测试 fileInDir", () => {
-        let result = options.fileInDir("example/testdata");
+        let result = fileops.fileInDir("testdata");
         it("返回类型是数组?", () => {
             expect(result).to.be.an("array");
         });
         it("搜寻出了当前目录下所有文件?", () => {
-            expect(result).to.include("example/testdata/input.json");
-            expect(result).to.include("example/testdata/output.md");
+            expect(result).to.include("testdata/input.json");
+            expect(result).to.include("testdata/output.md");
         });
         it("没有搜寻出更深层目录下的文件?", () => {
-            expect(result).to.not.include("example/testdata/deeper/data.json");
+            expect(result).to.not.include("testdata/deeper/data.json");
         });
     });
 
     describe("函数测试 deepFileInDir", () => {
-        let result = options.deepFileInDir("example/testdata");
+        let result = fileops.deepFileInDir("testdata");
         it("返回类型是数组?", () => {
             expect(result).to.be.an("array");
         });
         it("搜寻出了当前目录下所有文件?", () => {
-            expect(result).to.include("example/testdata/input.json");
-            expect(result).to.include("example/testdata/output.md");
+            expect(result).to.include("testdata/input.json");
+            expect(result).to.include("testdata/output.md");
         });
         it("搜寻出了更深层目录下的文件?", () => {
-            expect(result).to.include("example/testdata/deeper/data.json");
+            expect(result).to.include("testdata/deeper/data.json");
         });
     });
 
     describe("函数测试 subdirInDir", () => {
-        let result = options.subdirInDir("example/testdata");
+        let result = fileops.subdirInDir("testdata");
         it("返回类型是数组?", () => {
             expect(result).to.be.an("array");
         });
         it("没有搜寻出文件?", () => {
-            expect(result).to.not.include("example/testdata/input.json");
-            expect(result).to.not.include("example/testdata/output.md");
+            expect(result).to.not.include("testdata/input.json");
+            expect(result).to.not.include("testdata/output.md");
         });
         it("搜寻到了当前目录下的所有子目录?", () => {
-            expect(result).to.include("example/testdata/deeper");
+            expect(result).to.include("testdata/deeper");
         });
     });
 
     describe("函数测试 replaceExt", () => {
         it("普通替换", () => {
-            expect(options.replaceExt("index.json", ".js")).is.equal("index.js");
+            expect(fileops.replaceExt("index.json", ".js")).is.equal(
+                "index.js"
+            );
         });
         it("大小写敏感", () => {
-            expect(options.replaceExt("main.c", ".C")).is.equal("main.C");
+            expect(fileops.replaceExt("main.c", ".C")).is.equal("main.C");
         });
         it("无后缀替换成有后缀", () => {
-            expect(options.replaceExt("README", ".md")).is.equal("README.md");
+            expect(fileops.replaceExt("README", ".md")).is.equal("README.md");
         });
         it("相同后缀替换", () => {
-            expect(options.replaceExt("README.md", ".md")).is.equal(
+            expect(fileops.replaceExt("README.md", ".md")).is.equal(
                 "README.md.md"
             );
         });
