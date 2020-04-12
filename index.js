df09d6731ec947eb72308d0fe987d4b49ed437fa
Jaycee Chow <2832203391@qq.com>
2020-04-12 15:00:59 +0800

    v0.2.4 更新冲突处理

--- index.js
+++ index.js
@@ -1,73 +1,72 @@
 const fs = require("fs");
 const fileops = require("./lib/fileops");
 const core = require("./lib/core");
+const numberGenerate = require("./lib/generator").numberGenerate;
+const pathPack = require("./lib/pathops").pathPack;
+const pathSwitch = require("./lib/pathops").pathSwitch;
 
 //导入配置
 const { origin, data, view } = require("./config.json");
 
-/*
-包装path字符串，目的是限制所有的非项目文件到一个特定的目录下，做到工程与使用的分离
-pre设置为data意味着所有的origin、data、view文件都会被放到一个data子目录中
-*/
-function pathPack(path, pre = "data") {
-    return `${pre}/${path}`;
-}
-
-/*
-为了方便转换模式的时候变换path字符串
-对于mode参数的说明：
-0，代表path的格式应该切换到origin mode
-1，代表path的格式应该切换到data mode
-2，代表path的格式应该切换到view mode
-*/
-function pathSwitch(source, mode = 0) {
-    modemap = [`${origin.storage}`, `${data.storage}`, `${view.storage}`];
-    if (
-        /^(data)\/(.*)\/(.*\..*)$/.test(source) ||
-        /^(data)(\/)(.*\..*)$/.test(source)
-    )
-        return `${RegExp.$1}/${modemap[mode]}/${RegExp.$3}`;
-}
-
+//定义一些变量，方便在workflow代码的重用
 var mode = [origin, data, view],
     name = ["origin", "data", "view"],
     behavior = [core.formData, core.dataView, null];
 
-console.log("检查文件");
+//检查需要的目录是否被创建成功
+console.log("check file");
 for (let check of mode) {
     let path = pathPack(check.storage);
     fileops.mkdirs(path);
 }
-console.log("检查完毕\n");
+console.log("check finshed.\n");
 
+//使用Promise来定义一个workflow
 workflow = new Promise((resolve, reject) => {
     resolve();
 });
 
+//workflow主循环
 for (let i = 0; i < mode.length - 1; i++) {
     workflow = workflow.then(() => {
         var nextSet = [];
+        //确认流开关是否开启
         if (mode[i].switch) {
             console.log(`\nworkflow: ${name[i]}->${name[i + 1]}...`);
+            //从模式存储路径获得所有文件
             var fileSet = fileops
                 .fileInDir(pathPack(mode[i].storage))
                 .filter((file) => RegExp(`${mode[i].template}$`).test(file));
+            //并入其他汇合流，单文件合并
             mode[i].extra.dir.forEach((dir) => {
                 fileops.fileInDir(dir).forEach((file) => {
                     fileSet.push(file);
                 });
             });
+            //并入其他汇合流，多文件合并(目录合并)
             mode[i].extra.file.forEach((file) => {
                 fileSet.push(file);
             });
+            //获得流的下一个状态
             nextSet = fileSet.map((file) => {
+                console.log(file);
                 return fileops.replaceExt(
                     pathSwitch(file, i + 1),
                     mode[i + 1].template
                 );
             });
+            //处理流
             for (let j = 0; j < nextSet.length; j++) {
+                //冲突检测与处理，通过不断在流的后面添加一个递增数字
+                let iter = numberGenerate();
+                while (fs.existsSync(nextSet[j])) {
+                    nextSet[j] = nextSet[j].replace(
+                        /([0-9]*)\.(.*)$/,
+                        `${iter()}.$2`
+                    );
+                }
                 console.log(`from ${fileSet[j]} to ${nextSet[j]}`);
+                //写入下一个文件流
                 behavior[i](fileSet[j], nextSet[j]);
             }
             console.log("next work.\n");
