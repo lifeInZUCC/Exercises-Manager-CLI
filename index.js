41213a361832f737328c8343c207fa96718e04f6
Jaycee Chow <2832203391@qq.com>
2020-04-27 12:23:33 +0800

    修复了extra区域文件导入的bug

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const core = require("./lib/core");
+const process = require("process");
 
 //workflow主循环
 var workflow = new core.Workflow();
 workflow.loop();
+
+process.on("unhandledRejection", (error) => {
+    console.error("unhandledRejection", error);
+    process.exit(1); // To exit with a 'failure' code
+});
--- init.js
+++ init.js
@@ -1,48 +1,16 @@
 const fs = require("fs");
 const path = require("path");
 const figlet = require("figlet");
+const config = require("./config.json");
 
 console.log(figlet.textSync("Exercises Manager"));
-
-var Config = {
-    origin: {
-        switch: true,
-        template: "txt",
-        storage: "origin",
-        extra: {
-            file: [],
-            dir: [],
-        },
-    },
-    data: {
-        switch: true,
-        template: "json",
-        storage: "data",
-        extra: {
-            file: [],
-            dir: [],
-        },
-    },
-    view: {
-        template: "md",
-        storage: "view",
-    },
-    viewformat: {
-        titleLevel: "###",
-        tableHeader: "|编号|选项|",
-        tableNeck: "|:-|:-|",
-        colorMark: '<font color="red">',
-    },
-};
-
-fs.writeFileSync("config.json", JSON.stringify(Config, null, "\t"));
 
 //检查需要的目录是否被创建成功
 console.log("create file");
 for (let subdir of [
-    Config.origin.storage,
-    Config.data.storage,
-    Config.view.storage,
+    config.origin.storage,
+    config.data.storage,
+    config.view.storage,
 ]) {
     let dir = path.join("data", subdir);
     fs.mkdirSync(dir, { recursive: true });
