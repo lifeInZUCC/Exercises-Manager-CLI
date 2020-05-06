4b131becacbe8282492ce7a7546b0949b4c2cae4
Jaycee Chow <2832203391@qq.com>
2020-05-06 15:07:33 +0800

    更新mooc脚本

--- chrome/js/mooc.js
+++ chrome/js/mooc.js
@@ -26,72 +26,133 @@ function saveJSON(filename, data) {
     a.dispatchEvent(event);
 }
 
+function handleRadio(problem) {
+    var problemObject = { title: "", choice: [], answer: "" };
+    problemObject.title = problem
+        .querySelector("div.j-title div.f-richEditorText")
+        .textContent.trim();
+    choices = problem.querySelectorAll("div.j-choicebox .choices label.f-cb");
+    for (let choice of choices) {
+        problemObject.choice.push({
+            option: choice
+                .querySelector("div.optionPos")
+                .textContent.replace(".", ""),
+            content: choice
+                .querySelector("div.f-richEditorText")
+                .textContent.trim(),
+        });
+    }
+    try {
+        problemObject.answer = problem
+            .querySelector("div.analysisInfo span.tt2")
+            .textContent.trim();
+    } catch (error) {
+        console.log(error);
+        problemObject.answer = "undefine";
+    }
+    console.log(problemObject);
+    return problemObject;
+}
+function handleCheckBox(problem) {
+    var problemObject = { title: "", choice: [], answer: "" };
+    problemObject.title = problem
+        .querySelector("div.j-title div.f-richEditorText")
+        .textContent.trim();
+    choices = problem.querySelectorAll("div.j-choicebox .choices label.f-cb");
+    for (let choice of choices) {
+        problemObject.choice.push({
+            option: choice
+                .querySelector("div.optionPos")
+                .textContent.replace(".", ""),
+            content: choice
+                .querySelector("div.f-richEditorText")
+                .textContent.trim(),
+        });
+    }
+    try {
+        problemObject.answer = problem
+            .querySelector("div.analysisInfo span.tt2")
+            .textContent.trim()
+            .split("、")
+            .join(" ");
+    } catch (error) {
+        console.log(error);
+        problemObject.answer = "undefine";
+    }
+    console.log(problemObject);
+    return problemObject;
+}
+function handleSimpleRadio(problem) {
+    var problemObject = { title: "", choice: [], answer: "" };
+    problemObject.title = problem
+        .querySelector("div.j-title div.f-richEditorText")
+        .textContent.trim();
+    choices = problem.querySelectorAll("div.j-choicebox .choices label.f-cb");
+    for (let choice of choices) {
+        console.log(choice.querySelector("div.f-richEditorText"));
+        problemObject.choice.push({
+            option: choice
+                .querySelector("div.optionPos")
+                .textContent.replace(".", ""),
+            content:
+                choice.querySelector(
+                    "div.f-richEditorText span.u-icon-wrong"
+                ) === null
+                    ? "T"
+                    : "F",
+        });
+    }
+    try {
+        problemObject.answer = problem
+            .querySelector("div.analysisInfo span.tt2")
+            .textContent.trim();
+    } catch (error) {
+        console.log(error);
+        problemObject.answer = "undefine";
+    }
+    console.log(problemObject);
+    return problemObject;
+}
+function handleText(problem) {
+    var problemObject = { title: "", answer: "" };
+    problemObject.title = problem
+        .querySelector("div.j-title div.f-richEditorText")
+        .textContent.trim();
+    choices = problem.querySelectorAll("div.j-choicebox .choices label.f-cb");
+    try {
+        problemObject.answer = problem
+            .querySelector("div.analysisInfo span.tt2")
+            .textContent.trim();
+    } catch (error) {
+        console.log(error);
+        problemObject.answer = "undefine";
+    }
+    console.log(problemObject);
+    return problemObject;
+}
+
 (function () {
     if (!/icourse163/.test(window.location.href)) {
         window.alert("检测到不符合目标网站");
         return;
     }
-    var topic = [];
-    var options = ["A", "B", "C", "D"];
-    for (
-        let i = 0;
-        i < document.getElementsByClassName("m-choiceQuestion").length;
-        i++
-    ) {
-        let temp = { topic: "", choice: [], answer: "" };
-        temp.topic = document
-            .getElementsByClassName("m-choiceQuestion")
-            [i].getElementsByClassName("j-title")[0]
-            .getElementsByClassName("f-richEditorText")[0].textContent;
-        for (
-            let j = 0;
-            j <
-            document
-                .getElementsByClassName("m-choiceQuestion")
-                [i].getElementsByClassName("j-choicebox")[0]
-                .getElementsByClassName("f-richEditorText").length;
-            j++
-        ) {
-            if (
-                document
-                    .getElementsByClassName("m-choiceQuestion")
-                    [i].getElementsByClassName("j-choicebox")[0]
-                    .getElementsByClassName("f-richEditorText").length == 2
-            ) {
-                temp.choice.push({
-                    option: options[j],
-                    content:
-                        document
-                            .getElementsByClassName("m-choiceQuestion")
-                            [i].getElementsByClassName("j-choicebox")[0]
-                            .getElementsByClassName("f-richEditorText")
-                            [j].getElementsByClassName("u-icon-wrong").length ==
-                        0
-                            ? "T"
-                            : "F",
-                });
-            } else {
-                temp.choice.push({
-                    option: options[j],
-                    content: document
-                        .getElementsByClassName("m-choiceQuestion")
-                        [i].getElementsByClassName("j-choicebox")[0]
-                        .getElementsByClassName("f-richEditorText")[j]
-                        .textContent,
-                });
-            }
+    var problemSet = document.querySelectorAll(".u-questionItem");
+    var problemObjectSet = [];
+    for (let problem of problemSet) {
+        switch (problem.querySelector(".qaCate span").textContent) {
+            case "单选":
+                problemObjectSet.push(handleRadio(problem));
+                break;
+            case "多选":
+                problemObjectSet.push(handleCheckBox(problem));
+                break;
+            case "判断":
+                problemObjectSet.push(handleSimpleRadio(problem));
+                break;
+            case "填空":
+                problemObjectSet.push(handleText(problem));
+                break;
         }
-        try {
-            temp.answer = document
-                .getElementsByClassName("m-choiceQuestion")
-                [i].getElementsByClassName("j-choicebox")[0]
-                .getElementsByClassName("tt2")[0].textContent;
-        } catch (err) {
-            console.log(err);
-            temp.answer = "undefine";
-        }
-        topic.push(temp);
     }
-
-    saveJSON("data.json", JSON.stringify(topic, null, "\t"));
+    saveJSON("data.json", JSON.stringify(problemObjectSet, null, "\t"));
 })();
